8ac395c27feec129bfd9c86d15c5e1744159e3fd
astoorangi <astoorangi@outlook.com>
2018-11-24 17:28:13 +0100

    Search Bar works!

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,15 +1,29 @@
 function getPos() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(checkPos);
+        navigator.geolocation.getCurrentPosition(convert);
     }
+}
+
+function convert(input) {
+    let lat = input.coords.latitude;
+    let lon = input.coords.longitude;
+    checkPos(lat + ',' + lon)
+}
+
+function readInput() {
+    let inputElement = document.getElementById("searchbar");
+    let inputContent = inputElement.value;
+    console.log(inputElement);
+    checkPos(inputContent);
 }
 
 async function checkPos(search) {
     try {
-        const request = await fetch('https://nominatim.openstreetmap.org/search/?format=json&limit=10&q=' + search.coords.latitude + ',' + search.coords.longitude);
+        const request = await fetch('https://nominatim.openstreetmap.org/search/?format=json&limit=10&q=' + search);
         const data = await request.json();
 
-        console.log(data[0]["display_name"]);
+        let inputElement = document.getElementById("searchbar");
+        inputElement.value = data[0]["display_name"];
     } catch (e) {
         console.error('fetch error', e);
     }
