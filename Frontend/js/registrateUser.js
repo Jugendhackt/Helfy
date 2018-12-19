7223ed26dac4dd652640702aa56965cc99bfdc9c
jaybeejs <you@example.com>
2018-12-19 17:43:58 +0100

    added communication with backend

--- Frontend/js/registrateUser.js
+++ Frontend/js/registrateUser.js
@@ -2,10 +2,29 @@
 Registration
 */
 
+var server_url = "";
+
+
+async function existsUser() {
+    var fullurl = server_url + '/backend/index.php?request=existsUser&username' + username;
+    try {
+        const request = await fetch(fullurl, {
+            method: "GET",
+            dataType: "application/x-www-form-urlencoded",
+        });
+        const data = await request.text();
+        document.getElementById("answer").value = data;
+        console.log("fetch success");
+
+
+    } catch (e) {
+        console.error('fetch error', e);
+    }
+
+}
+
 
 async function registrateUser() {
-    var server_url = "";
-
     var username = document.getElementById("validationUsername").value;
     var password = document.getElementById("validationPassword").value;
     var passwordWdh = document.getElementById("validationPasswordWdh").value;
@@ -15,19 +34,49 @@ async function registrateUser() {
     var ort = document.getElementById("validationOrt").value;
     var plz = document.getElementById("validationPlz").value;
     var fullurl = server_url + '/backend/index.php?request=registrateUser&username=' + username + '&password=' + password + '&email=' + email + '&vname=' + vname + '&nname=' + nname + '&plz=' + plz + '&ort=' + ort;
-    if(password == passwordWdh){
+    if (password == passwordWdh) {
         try {
-        const request = await fetch(fullurl, {
-            method: "GET",
-            dataType: "application/x-www-form-urlencoded",
+            const request = await fetch(fullurl, {
+                method: "GET",
+                dataType: "application/x-www-form-urlencoded",
             });
             const data = await request.text();
             console.log("js running");
-        
-        
+            console.log(fullurl);
+
+
         } catch (e) {
             console.error('fetch error', e);
         }
     }
-  
+
+}
+
+
+async function registrateUserFromData(username, password, passwordWdh, email, vname, nname, ort, plz) {
+    var fullurl = server_url + '/backend/index.php?request=registrateUser&username=' + username + '&password=' + password + '&email=' + email + '&vname=' + vname + '&nname=' + nname + '&plz=' + plz + '&ort=' + ort;
+    if (password == passwordWdh) {
+        try {
+            const request = await fetch(fullurl, {
+                method: "GET",
+                dataType: "application/x-www-form-urlencoded",
+            });
+            const data = await request.text();
+            console.log(data);
+            if (data == "failed") {
+                console.log("registration failed");
+                document.getElementById("simpleoutput").innerHTML = "Der Benutzername ist bereits vergeben!";
+            } else {
+                console.log("registration success");
+                document.getElementById("simpleoutput").innerHTML = "Erfolgreich!<br><br>Sie bekommen nun eine E-Mail mit einem Bestätigungslink. Diesen müssen Sie öffnen um ihren Account nutzen zu können.";
+            }
+
+
+        } catch (e) {
+            console.error('fetch error', e);
+            document.getElementById("simpleoutput").innerHTML = "Die Verbindung zum Server ist fehlgeschlagen!";
+        }
+    } else {
+        document.getElementById("simpleoutput").innerHTML = "Die Passwörter sind nicht gleich!";
+    }
 }
