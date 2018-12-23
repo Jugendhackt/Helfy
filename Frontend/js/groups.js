e539b35eaa3b7bc347d20f732cd8b0bc1291c063
jaybeejs <45308098+jaybeejs@users.noreply.github.com>
2018-12-23 22:44:40 +0100

    connection to backend added

---
+++ Frontend/js/groups.js
@@ -0,0 +1,69 @@
+/*
+Create, edit and lookup groups
+*/
+
+var server_url = "https://example.url.com";
+
+
+var data = "";
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "helfy_" + cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+function getCookie(cname) {
+    var name = "helfy_" + cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+async function newGroup(tnum) {
+    var runon = false;
+    var name = document.getElementById("groupname").value;
+    var users = getCookie("username");
+    var description = document.getElementById("description").value;
+    runon = (name != "" && description != "");
+    for(i = 2; i <= tnum; i++){
+        users = users + "," + document.getElementById("validationUsername" + i).value;
+        if(document.getElementById("validationUsername" + i).value == ""){
+            runon = false;
+        }
+    }
+    if(runon){
+        var fullurl = server_url + '/backend/index.php?request=newGroup&username=' + getCookie("username") + "&session=" + getCookie("session") + "&users=" + users + "&groupname=" + name + "&description=" + description;
+        try {
+            let request = await fetch(fullurl, {
+                method: "GET",
+                dataType: "application/x-www-form-urlencoded",
+            });
+
+            data = await request.text();
+            console.log(data);
+            console.log("fetch success");
+            document.getElementById("errorline").innerHTML = "Gruppe erfolgreich erstellt!";
+            document.getElementById("errorline").style.color = "green";
+
+        } catch (e) {
+            console.error('fetch error', e);
+            data = "fetch_error";
+        }
+    } else {
+        document.getElementById("errorline").innerHTML = "Füllen Sie alle Felder aus!";
+        document.getElementById("errorline").style.color = "red";        
+    }
+}
